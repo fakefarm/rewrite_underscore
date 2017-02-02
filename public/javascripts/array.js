84af9a48bd760d91337c81d2e080ec0fed663f66
Dave Woodall <dave@woodalls.me>
2017-02-01 22:15:20 -0500

    first two specs pass, woot

--- public/javascripts/array.js
+++ public/javascripts/array.js
@@ -1,5 +1,10 @@
-var _ = {
-  first: function() {
-    return 1;
+var _ = (function(){
+  var _ = function() {}
+
+  _.first = function(array, n) {
+    if( array == null || array.length < 1 ) return void 0;
+    if(n == null) return array[0];
+    if(n == 0) return [];
   }
-}
+  return _;
+}());
