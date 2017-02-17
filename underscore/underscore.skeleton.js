6be41eebefb38b0a34415ff7a2a163f6c61d004d
woodall <dave@woodalls.me>
2017-02-17 16:25:15 -0500

    Notes about rest

--- underscore/underscore.skeleton.js
+++ underscore/underscore.skeleton.js
@@ -252,6 +252,12 @@
   };
 
   _.rest = _.tail = _.drop = function(array, n, guard) {
+    // _dw note
+    // seems that slice's api works that the second argument can either be a boolean or a number.
+    // if it's a boolean (true) then it will slice the first element
+    // if it's false, then it does not slice first element
+    // if it's a number, then it will slice the number of elements off the front and go until the end.
+
     return slice.call(array, n == null || guard ? 1 : n);
   };
 
